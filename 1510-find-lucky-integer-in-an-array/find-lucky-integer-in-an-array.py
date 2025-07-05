40a6f90596fc8e704a6e37392c75439a27f84f7b
kibot1 <sebastiancalzadillas39093@gmail.com>
2025-07-04 22:09:53 -0600

    Time: 10 ms (22.94%) | Memory: 12.5 MB (84.59%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -1,21 +1,18 @@
 class Solution(object):
     def findLucky(self, arr):
         lucky_number = -1
-        skip = []
+        
+        arr_map = {}
 
-        for x in arr:
-            if x in skip:
-                continue
-            temp = x
-            freq = 0
-            for y in arr:
-                if y == temp:
-                    freq += 1
-            if temp == freq and temp > lucky_number:
-                lucky_number = temp
+        for int in arr:
+            freq = 1
+            if arr_map.get(int) != None:
+                freq = freq + arr_map[int]
+            arr_map.update({int:freq})
 
-            freq = 0
-            skip.append(temp)
+        for key, value in arr_map.items():
+            if key == arr_map[key] and key > lucky_number:
+                lucky_number = key
 
         return lucky_number
-        
+         
